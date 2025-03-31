6e882fbad9b4972d2ba902efbd7b688422d2ed10
Ahmed Ashour <asashour@yahoo.com>
2025-03-31 13:02:47 +0200

    As a web application.

---
+++ src/main/java/abc/controller/DevelopmentController.java
@@ -0,0 +1,26 @@
+package abc.controller;
+
+import abc.dao.SomethingRepository;
+import abc.entity.Something;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class DevelopmentController {
+	private final SomethingRepository somethingRepository;
+
+	@GetMapping("test")
+	public void test() {
+		var something = new Something();
+		somethingRepository.save(something);
+	}
+
+}
