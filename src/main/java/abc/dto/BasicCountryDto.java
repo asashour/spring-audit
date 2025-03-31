825eb418b3b74f23a77124ae08bf23892627ab8e
Ahmed Ashour <asashour@yahoo.com>
2025-03-31 17:23:36 +0200

    Java 24.

---
+++ src/main/java/abc/dto/BasicCountryDto.java
@@ -0,0 +1,18 @@
+package abc.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BasicCountryDto {
+
+	private int id;
+
+	private String name;
+
+	private String isoCode;
+
+	private boolean otherGender;
+
+}
