d5029430f7f3c35424de81a6ffe7e872d41dcdbc
Ahmed Ashour <asashour@yahoo.com>
2025-03-31 15:24:46 +0200

    Not saving...

--- src/test/java/abc/repository/SomethingRepositoryTest.java
+++ src/test/java/abc/repository/SomethingRepositoryTest.java
@@ -1,11 +1,16 @@
 package abc.repository;
 
 import abc.entity.Something;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@ActiveProfiles(value = "test")
+@Transactional
+@Slf4j
 class SomethingRepositoryTest {
 
 	@Autowired
